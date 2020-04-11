d4bbf593c2e4976e29271fba68be32e10832c196
myjeepxx <myjeepxx@gmail.com>
2020-04-11 15:55:14 -0700

    js modified

--- Index.js
+++ Index.js
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
 <body>
-<p id="demo">he value of z is.</p>
+<p id="demox">he value of x is.</p>
+<p id="demoy">he value of y is.</p>
+<p id="demoz">he value of z is.</p>
 <script>
 var xx, yy, zz;
 xx = 5;
 yy= 6;
 zz = xx + yy;
-document.getElementById("demo").innerHTML ="The value of z is " + zz + ".";
+document.getElementById("demox").innerHTML ="The value of z is " + xx + ".";
+document.getElementById("demoy").innerHTML ="The value of z is " + yy + ".";
+document.getElementById("demoz").innerHTML ="The value of z is " + zz + ".";
 </script>
 </body>
 </html>
