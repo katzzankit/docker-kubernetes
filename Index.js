f49e529c18a6edb3a71ea4ca9708f41eb445afd4
myjeepxx <myjeepxx@gmail.com>
2020-04-11 15:17:59 -0700

    Done JS

--- Index.js
+++ Index.js
@@ -3,11 +3,11 @@
 <body>
 <p id="demo">he value of z is.</p>
 <script>
-var xx, y, z;
+var xx, y, zz;
 xx = 5;
 y = 6;
-z = xx + y;
-document.getElementById("demo").innerHTML ="The value of z is " + z + ".";
+zz = xx + y;
+document.getElementById("demo").innerHTML ="The value of z is " + zz + ".";
 </script>
 </body>
 </html>
