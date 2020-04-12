bfe7ae5c51ed0bfe73ba7adc20b149fccfc68d6b
myjeepxx <myjeepxx@gmail.com>
2020-04-11 19:10:41 -0700

    Update Index.js
    Change one moer time.

--- Index.js
+++ Index.js
@@ -5,6 +5,8 @@
 <p id="demoyy">he value of y is.</p>
 <p id="demozz">he value of z is.</p>
 <script>
+
+
 var xx, yy, zz;
 xx = 5;
 yy= 6;
@@ -12,6 +14,12 @@ zz = xx + yy;
 document.getElementById("demoxx").innerHTML ="The value of z is " + xx + ".";
 document.getElementById("demoyy").innerHTML ="The value of z is " + yy + ".";
 document.getElementById("demozz").innerHTML ="The value of z is " + zz + ".";
+
+var i = 0;
+forEach((item, i) => {
+  
+});
+
 </script>
 </body>
 </html>
